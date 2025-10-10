9350329f2f18f81d0b44d249acd0e82c8a71ef23
palmhillstry <mohamedgamesar4321@gmail.com>
2025-10-10 06:13:14 +0300

    home activity (pt1 unfinished)

---
+++ app/src/main/java/com/example/noxnews/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.example.noxnews
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.noxnews.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btn1.setOnClickListener {
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/noxnews/NewsCallable.kt
+++ app/src/main/java/com/example/noxnews/NewsCallable.kt
@@ -1,12 +1,14 @@
 package com.example.noxnews
 
+import android.R
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface NewsCallable {
 
-    @GET("/v2/top-headlines?country=us&category=general&apiKey=e145d0ed4d974df59b294a451d4d4e96&pageSize=60")
-    fun getNews(): Call<News>
+    @GET("/v2/top-headlines?country={countryID}&category={categoryID}&apiKey=e145d0ed4d974df59b294a451d4d4e96&pageSize=60")
+    fun getNews(@Path("countryID") countryID : String , @Path("categoryID") categoryID : String): Call<News>
 
 
 }
