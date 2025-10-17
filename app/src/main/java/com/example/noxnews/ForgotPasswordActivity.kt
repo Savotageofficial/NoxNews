e0009736e9aa606006c093bf57220c94873169fc
palmhillstry <mohamedgamesar4321@gmail.com>
2025-10-17 19:53:48 +0300

    ForgotPasswordActivity.kt created

---
+++ app/src/main/java/com/example/noxnews/ForgotPasswordActivity.kt
@@ -0,0 +1,48 @@
+package com.example.noxnews
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.noxnews.databinding.ActivityForgotPasswordBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        lateinit var auth: FirebaseAuth
+        auth = Firebase.auth
+
+
+        binding.submitBtn.setOnClickListener {
+
+            var emailAddress = binding.emailEt.text.toString()
+
+            auth.sendPasswordResetEmail(emailAddress)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Password reset email sent successfully
+                        Toast.makeText(this, "Password reset email sent to $emailAddress", Toast.LENGTH_SHORT).show()
+                        finish() // Go back to the login screen
+                    } else {
+                        // Handle the error (e.g., email not found, network issues)
+                        Toast.makeText(this, "Error sending password reset email: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+
+    }
+}
